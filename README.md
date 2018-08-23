caf95107ccef8f9da6ffc977cf1618394416da6d
Luke Pahler <luke.pahler@cgi.com>
2018-08-23 09:41:37 -0600

    Adding helper script to do curls to config server More Readme New company config

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # configuration-depot
 How I made the service
 $ cf create-service p-config-server trial configuration-depot-service -c ./config-server.json
+
+How to get the four variables
+$ cf service-key configuration-depot-service configuration-depot-service-key
---
+++ getToken.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo getting token...
+
+uri=https://config-dbdc31e5-0eeb-41ed-8d1a-6244d0e4a4df.cfapps.io
+access_token_uri=https://p-spring-cloud-services.uaa.run.pivotal.io/oauth/token
+client_id=p-config-server-60f69297-1fcf-4191-b84a-90db843ef379
+client_secret=5Y8mL0ZkIHnT
+
+token=$(curl $access_token_uri -u $client_id:$client_secret -d grant_type=client_credentials | jq -r .access_token);
+
+#echo $token
+echo curling the config server with said token
+curl -H "Authorization: bearer $token" -H "Accept: application/json" $uri/app-name/profile | jq
+
+echo Doing company specific request
+curl -H "Authorization: bearer $token" -H "Accept: application/json" $uri/company/cloud | jq
+
